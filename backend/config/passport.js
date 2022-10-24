1e77992030a44012b20730377fc906945a46e8d3
acoop68@gmail.com <acoop68@gmail.com>
2022-10-23 22:10:55 -0400

    csrf token

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,8 +3,31 @@ const LocalStrategy = require('passport-local');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const jwt = require('jsonwebtoken');
+const { secretOrKey } = require('./keys');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = secretOrKey;
 
-passport.use(new LocalStrategy({
+exports.loginUser = async function(user) {
+    const userInfo = {
+      _id: user._id,
+      username: user.username,
+      email: user.email
+    };
+    const token = await jwt.sign(
+      userInfo, // payload
+      secretOrKey, // sign with secret key
+      { expiresIn: 3600 } // tell the key to expire in one hour
+    );
+    return {
+      user: userInfo,
+      token
+    };
+  };
+
+  passport.use(new LocalStrategy({
     session: false,
     usernameField: 'email',
     passwordField: 'password',
@@ -18,3 +41,28 @@ passport.use(new LocalStrategy({
     } else
       done(null, false);
   }));
+
+  passport.use(new JwtStrategy(options, async(jwtPayload, done) => {
+    try {
+        const user = await User.findById(jwtPayload._id)
+        if (user) {
+            return done(null, user);
+        }
+
+        return done(null, false)
+    }
+    catch (err) {
+        done(err);
+    }
+}));
+
+exports.requireUser = passport.authenticate('jwt', { session: false });
+
+exports.restoreUser = (req, res, next) => {
+    return passport.authenticate('jwt', { session: false }, function(err, user) {
+      if (err) return next(err);
+      if (user) req.user = user;
+      next();
+    })(req, res, next);
+  };
+
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
-
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,41 +16,28 @@ router.get('/', function(req, res, next) {
 
 //POST /api/users/register
 router.post('/register', async (req, res, next) => {
+  // Check to make sure no one has already registered with the proposed email or
+  // username.
   const user = await User.findOne({
-    $or: [{ email: req.body.email}, {username: req.body.username}]
+    $or: [{ email: req.body.email }, { username: req.body.username }]
   });
 
-
-//POST /api/users/login
-router.post('/login', async(req, res, next) => {
-  passport.authenticate('local', async function(err, user) {
-    if (err) return next(err);
-    if (!user) {
-      const err = new Error('Invalid credentials');
-      err.statusCode = 400;
-      err.errors = { email: "Invalid credentials" };
-      return next(err);
-    }
-    return res.json({ user });
-  })(req, res, next);
-});
-
-  //handle erros if user exists
   if (user) {
+    // Throw a 400 error if the email address and/or username already exists
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
     if (user.email === req.body.email) {
-      errors.email = "A user has already registered with this email"
+      errors.email = "A user has already registered with this email";
     }
     if (user.username === req.body.username) {
-      errors.username = "A user has already registered with this username"
+      errors.username = "A user has already registered with this username";
     }
     err.errors = errors;
     return next(err);
   }
 
-  //create a new user if user doesnt already exist
+  // Otherwise create a new user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email
@@ -62,12 +50,43 @@ router.post('/login', async(req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       }
       catch(err) {
         next(err);
       }
-    });
+    })
+  });
+});
+
+// POST /api/users/login
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+  })(req, res, next);
+});
+
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
   });
 });
 
